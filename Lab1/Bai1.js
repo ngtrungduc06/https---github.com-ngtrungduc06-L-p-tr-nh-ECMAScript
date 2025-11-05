1218e3c48b906c0d551d79ca2b290c4652b6d18d
tduc-byte <nguyenthetrungduc2@gmail.com>
2025-11-05 14:25:52 +0700

    new file:   Lab1/Bai1.js

---
+++ Lab1/Bai1.js
@@ -0,0 +1,16 @@
+function calculateAPI(weight, height) {
+    const bmi = weight / (height * height);
+    let category;
+    if (bmi < 18.5) {
+        category = "Thiếu cân";
+    } else if (bmi < 25) {
+        category = "Bình thường";
+    } else if (bmi < 30) {
+        category = "Thừa cân";
+    } else {
+        category = "Béo phì";
+    }
+
+    return `BMI: ${bmi.toFixed(2)} - Phân loại: ${category}`;
+}
+console.log(calculateAPI(70, 1.75));

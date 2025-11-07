58cba6153fb6eec447446a88d15f96837706f56c
tduc-byte <nguyenthetrungduc2@gmail.com>
2025-11-07 15:57:43 +0700

    ...

--- Lab3/Baitap.js
+++ Lab3/Baitap.js
@@ -1,20 +1,4 @@
 // Bài 1:
-// Function 1
-function multiply(a, b) {
-    return a * b;
-}
-// Function 2
-function isPositive(number) {
-    return number >= 0;
-}
-// Function 3
-function getRandomNumber() {
-    return Math.random();
-}
-// Function 4
-document.addEventListener("click", function () {
-    console.log("Clicked!");
-});
 // Chuyển đổi thành arrow functions:
 
 // Function 1
